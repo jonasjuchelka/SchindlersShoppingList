10aa6aaa9d2b47f88e3073d46b71af2baab418b7
go57var <go57var@mytum.de>
2026-02-22 01:34:50 +0100

    docs: Update the general code

--- app.py
+++ app.py
@@ -3,8 +3,7 @@ from flask import Flask, render_template
 app = Flask(__name__)
 
 
-# That is a new comment.
 @app.route("/")
 def home():
-    return render_template("index.html")
+    return render_template("startPage.html")
 
